515dde74f0ee66a5b92bab135d8414a32a0d5878
Praveen_nischal4 <praveennischal12@gmail.com>
2022-10-16 23:50:47 +0530

    done

--- Hello.java
+++ Hello.java
@@ -3,11 +3,12 @@ class Hello
    public static void main(String [] args)
    {
     int age =29;
-   if(age > =18)
+   if(age >=18)
     {
-   System.out.println("eligible for voting");
+   System.out.println("eligible to voting");
  }else{
-System.out.println("not eligible");
+System.out.println("no eligible");
+System.out.println("sussess");
 }
 
  }
