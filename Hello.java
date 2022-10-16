0fe4302a1af561352895520214cbc5fc78cc7e0e
Praveen_nischal4 <praveennischal12@gmail.com>
2022-10-16 20:03:09 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello
+ {
+   public static void main(String [] args)
+   {
+    int age =29;
+   if(age > =18)
+    {
+   System.out.println("eligible for voting");
+ }else{
+System.out.println("not eligible");
+}
+
+ }
+}
